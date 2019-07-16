284d58af1926968651aaa1962859a0a1f3a09b61
haowenchao <haowenchao@haowenchaos-mbp.cn.ibm.com>
2019-07-16 15:20:57 +0800

    wuliao

---
+++ shape.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+t = tf.constant([[[1, 1, 1], [2, 2, 2]], [[3, 3, 3], [4, 4, 4]]])
+tensor_shape = tf.shape(t)
+
+with tf.Session() as sess:
+	print(sess.run(tensor_shape))
