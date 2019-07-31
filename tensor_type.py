7d5e2b19f3b2473a4fcf196d47ce134d2ae59490
haowenchao <haowenchao123@gmail.com>
2019-07-31 12:55:10 +0800

    tensorflow basic

---
+++ tensor_type.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+
+n0 = np.array(20, dtype = np.int32)
+n1 = np.array([b"Tensor", b"flow", b"is", b"great"])
+n2 = np.array([[True, False, False],
+                [False, True,False]], 
+                dtype = np.bool)
+tensor0D = tf.Variable(n0, name = "t_0")
+tensor1D = tf.Variable(n1, name = "t_1")
+tensor2D = tf.Variable(n2, name = "t_2")
+init_Op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init_Op)
+    print(sess.run(tensor0D))
+    print(sess.run(tensor1D))
+    print(sess.run(tensor2D))
--- tf_datatype.py
+++ tf_datatype.py
@@ -1,1 +1,17 @@
 import tensorflow as tf
+import numpy as np
+
+tensor_0 = 1
+tensor_1 = [b"Tensor", b"flow", b"is", b"great"]
+tensor_2 = [[False, True, False], [True, True, False]]
+tensor_3 = [[[0, 0, 0], [0, 0, 1]], [[1, 0, 0], [1, 0, 1]], [[2, 0, 0], [2, 0, 1]]]
+
+print(type('Hello, Tensorflow!'))
+print(type(tf.constant('Hello, Tensorflow!')))
+print(type(tf.Session().run(tf.constant('Hello, Tensorflow!'))))
+
+print(np.int8==tf.int8)
+print(np.int16==tf.int16)
+print(np.int64==tf.int64)
+
+
